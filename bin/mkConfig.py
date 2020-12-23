9fb6c3352d13152cbec74f5f7684e63bc82f1ae2
LutzGross <l.gross@uq.edu.au>
2020-12-23 18:33:22 +1000

    configuration file generator added

---
+++ bin/mkConfig.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+fingal - program to set an initial configuration file.
+"""
+import argparse, os
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description='This creates an initial configuration file that needs to be edited', epilog="fingal - l.gross@uq.edu.au, version 23/12/2020")
+parser.add_argument(dest='project', metavar='PROJECT', type=str, help='configuration file name (py extension will be added.)')
+parser.add_argument('--compact', '-c',  dest='compact', action='store_true', default=False, help="only basic commends are added to file.")
+parser.add_argument('--station', '-s',  dest='stationfile', type=str, default=None, help="name of station file")
+parser.add_argument('--data', '-d',  dest='datafile', type=str, default=None, help="name of data file")
+parser.add_argument('--ipole', '-i',  dest='ipole', action='store_true', default=False, help="injections are pole")
+parser.add_argument('--mpole', '-m',  dest='mpole', action='store_true', default=False, help="measurements are pole")
+parser.add_argument('--rerror', '-r',  dest='rerror', action='store_true', default=False, help="resistence/electric field has error")
+parser.add_argument('--eerror', '-e',  dest='eerror', action='store_true', default=False, help="chargeability has error")
+
+args = parser.parse_args()
+
+out=""
+for line in open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "config_template.txt"), 'r').readlines():
+        if not (args.compact and line.startswith("#.")):
+            out+=line
+            
+
+CHANGE= {
+"created" :  datetime.now().strftime("%d.%m.%Y %H:%M"),
+"project" : args.project,
+"meshfile" : args.project+".fly",
+"stationfile" : args.stationfile,
+"stationsFMT" : "s%s",
+"datafile" : args.datafile,
+"dipoleInjections" : not args.ipole,
+"dipoleMeasurements" : not args.mpole,
+"hasResistanceError" : args.rerror,
+"hasEtaError" : args.eerror,
+"alpha1" : 10.
+}
+
+
+FN=args.project+".py"
+open(FN, 'w').write(out.format(**CHANGE))
+print(f"Configuration file {FN} created.")
+print(f"Please check the content of the file.")
+
