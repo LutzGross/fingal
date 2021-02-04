c119ca36b633bdaa5b91f60db8c3bb1f4c720a22
LutzGross <l.gross@uq.edu.au>
2021-02-04 15:50:11 +1000

    fixes

--- examples/VolcanoFW/config.py
+++ examples/VolcanoFW/config.py
@@ -12,47 +12,48 @@ datafile='Data.csv'
 schedulefile='FWSchedule.csv' # for synthetic cases
 dipoleMeasurements=False
 dipoleInjections=True
-useLogDefect=True
-sigma_background=0.01
+weightLogDefect=0.5
+clip_property_function=10
 
 restartfile='sigmarestart'
 tolerance=1e-4
+imax=500
+truncation=20
+restart=60
+interpolation_order=3
+core=[ "R1", "R2"]
+
 region_fixed = None
-useLogDefect=True
 
 w1=0
-w0eta=0
+w0=1e-15
+alpha0=1.
 alpha1=(570)**2/2.
 
 
-alpha0=1.
-w0=1e-13
-
-useLogDefectForEta=False
-useDifferenceOfFields=True
-
-
-alpha1eta=95000.
-alpha0eta=1.
-w1eta=1.e-4
-beta=0.1
+w1gamma=1e-8
+w0gamma=0
+alpha0gamma=alpha0
+alpha1gamma=alpha1
 
 outfile='sigma'
-#eta0=0.001
-#sigma0=0.01
-#sigma_true={1: sigma_background, 2 : 0.01, 3 : 0.1}
-#eta_true={1: eta_background, 2 : 0.001, 3 : 0.2}
 
-#w0=0
-#w0eta=0
-#alpha1=95000
-#alpha0=1.
-#w1=5e-8
-#useLogDefectForEta=False
-#useDifferenceOfFields=True
-#alpha1eta=95000.
-#alpha0eta=1.
-#w1eta=1.e-4 
-#beta=0.1
-#outfile='sigma'
-interpolation_order=1
+sigma0=0.001
+eta0=0.005
+sigma_background=sigma0
+eta_background=eta0
+
+
+
+def true_properties(domain):
+    from esys.escript import Scalar, Function
+    sigma_true=Scalar(sigma_background, Function(domain))
+    sigma_true.setTaggedValue("R1", 0.01)
+    sigma_true.setTaggedValue("R2", 0.1)
+    
+    gamma_background=eta_background/(1-eta_background) 
+    gamma_true=Scalar(gamma_background, Function(domain))
+    gamma_true.setTaggedValue("R1", 0.02)
+    gamma_true.setTaggedValue("R2", 0.2)
+   
+    return sigma_true, gamma_true
